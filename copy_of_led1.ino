e83c703453947d73954e58c50f8b2fb4e23f0e09
Mar7ins <84845448+AlbertoMar7ins@users.noreply.github.com>
2022-12-24 14:27:43 -0300

    cod&circuit

---
+++ copy_of_led1.ino
@@ -0,0 +1,44 @@
+void setup()
+{
+  Serial.begin(9600);
+   pinMode(8, OUTPUT);
+   pinMode(9, OUTPUT);
+   pinMode(10, OUTPUT);
+   pinMode(13, OUTPUT);
+   pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(8, HIGH);
+   delay(1000);
+  digitalWrite(8, LOW);
+   delay(1000);
+  digitalWrite(9, HIGH);
+   delay(1000);
+  digitalWrite(9, LOW);
+  delay(1000);
+  digitalWrite(10, HIGH);
+   delay(1000);
+  digitalWrite(10, LOW);
+  delay(1000);
+  digitalWrite(11, HIGH);
+   delay(1000);
+  digitalWrite(11, LOW);
+   delay(1000);
+  digitalWrite(12, HIGH);
+   delay(1000);
+  digitalWrite(12, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+     delay(1000);
+ digitalWrite(13, LOW);
+  delay(1000); 
+  
+ 
+ 
+  
+  
+  
+}
